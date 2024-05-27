436c8fbdd8efa6e288aec657f3b251290a6029fb
안교남 <skadldldl123@gmail.com>
2024-05-27 11:42:19 +0900

    이미지 업로드

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: './', // 프로젝트의 루트 디렉토리 설정
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: 'index.html', // 상대 경로로 지정
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    }
+  }
+});
